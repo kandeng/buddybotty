1a52848d01785d42a31137ecd5abf692b8d69fc5
Kan Deng <kan.deng@qq.com>
2025-10-20 10:24:32 +0800

    Upload some files and subdirectories
    Upload some files and subdirectories. But not include `NEUIKit` subdirectory to avoid oversizing 100 files.

---
+++ src/tkim_20251019/pages_contact_user_team/pages/redirect/contact.vue
@@ -0,0 +1,14 @@
+<template>
+  <view></view>
+</template>
+
+<script>
+export default {
+  onLoad() {
+    // 重定向到分包中的实际页面
+    uni.redirectTo({
+      url: '/pages_contact_user_team/NEUIKit/pages/Contact/index'
+    })
+  }
+}
+</script>
